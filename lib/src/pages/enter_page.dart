4160d942e53c2ace35197efe6c3d60914b9a8041
Kevyn Melo <troy.esname@gmail.com>
2020-09-26 00:22:33 -0500

    third commit

--- lib/src/pages/enter_page.dart
+++ lib/src/pages/enter_page.dart
@@ -8,108 +8,118 @@ class EnterPage extends StatefulWidget {
 }
 
 class _EnterPageState extends State<EnterPage> {
+  var _screenSize;
   @override
   Widget build(BuildContext context) {
+    _screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      body: Stack(
-        children: [
-          backgroundLogin(),
-          Container(
-            height: 200.0,
-            width: double.infinity,
-            child: SvgPicture.asset(
-              'assets/images/studentss.svg',
+      body: SafeArea(
+        child: Stack(
+          children: [
+            backgroundLogin(),
+            Container(
+              height: _screenSize.height * 0.3,
+              width: double.infinity,
+              child: SvgPicture.asset(
+                'assets/images/studentss.svg',
+              ),
+            ),
+            Align(alignment: Alignment.bottomCenter, child: stackTitle()),
+            Align(
+                alignment: Alignment.bottomCenter,
+                child: contentStack(context)),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Stack contentStack(BuildContext context) {
+    return Stack(
+      children: [
+        ContainerUpRounded(
+          ht: 0.57,
+          raLeft: 35.0,
+          raRight: 35.0,
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 10.0),
+          padding: EdgeInsets.only(left: 25.0, right: 25.0, top: 25.0),
+          height: _screenSize.height * 0.55,
+          width: double.infinity,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text(
+                  'Recuerda usar tus nombres verdaderos completos para que la evaluacion sea procesada correctamente',
+                  style: TextStyle(
+                      fontFamily: 'quicksand', fontWeight: FontWeight.w600),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                TextField(
+                  style: TextStyle(fontFamily: 'quicksand'),
+                  decoration: InputDecoration(
+                    suffixIcon: Icon(Icons.people_outline),
+                    border: OutlineInputBorder(),
+                    labelText: 'Nombres Completos',
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                TextField(
+                  style: TextStyle(fontFamily: 'quicksand'),
+                  decoration: InputDecoration(
+                    suffixIcon: Icon(Icons.people_outline),
+                    border: OutlineInputBorder(),
+                    labelText: 'Apellidos Completos',
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                TextField(
+                  style: TextStyle(fontFamily: 'quicksand'),
+                  decoration: InputDecoration(
+                    suffixIcon: Icon(Icons.format_list_numbered),
+                    border: OutlineInputBorder(),
+                    labelText: 'Grado',
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                RaisedButton(
+                  onPressed: () {},
+                  child: Container(
+                      height: 50.0,
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: Center(
+                          child: Text(
+                        'INGRESAR',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'quicksand',
+                            fontWeight: FontWeight.bold),
+                      ))),
+                  shape: StadiumBorder(),
+                  color: Color(0xff00B0FF),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'Copyright @2020 App para Karen Todos los derechos reservados.',
+                  style: TextStyle(fontFamily: 'quicksand', fontSize: 12.0),
+                  textAlign: TextAlign.center,
+                )
+              ],
             ),
           ),
-          Align(alignment: Alignment.bottomCenter, child: stackTitle()),
-          Align(
-              alignment: Alignment.bottomCenter,
-              child: Stack(
-                children: [
-                  ContainerUpRounded(
-                    ht: 0.57,
-                    raLeft: 35.0,
-                    raRight: 35.0,
-                  ),
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 10.0),
-                    padding:
-                        EdgeInsets.only(left: 25.0, right: 25.0, top: 25.0),
-                    height: 410.0,
-                    width: double.infinity,
-                    child: Column(
-                      children: [
-                        Text(
-                          'Recuerda usar tus nombres verdaderos completos para que la evaluacion sea procesada correctamente',
-                          style: TextStyle(
-                              fontFamily: 'quicksand',
-                              fontWeight: FontWeight.w600),
-                        ),
-                        SizedBox(
-                          height: 20.0,
-                        ),
-                        TextField(
-                          decoration: InputDecoration(
-                            suffixIcon: Icon(Icons.people_outline),
-                            border: OutlineInputBorder(),
-                            labelText: 'Nombres Completos',
-                          ),
-                        ),
-                        SizedBox(
-                          height: 20.0,
-                        ),
-                        TextField(
-                          decoration: InputDecoration(
-                            suffixIcon: Icon(Icons.people_outline),
-                            border: OutlineInputBorder(),
-                            labelText: 'Apellidos Completos',
-                          ),
-                        ),
-                        SizedBox(
-                          height: 20.0,
-                        ),
-                        TextField(
-                          decoration: InputDecoration(
-                            suffixIcon: Icon(Icons.format_list_numbered),
-                            border: OutlineInputBorder(),
-                            labelText: 'Grado',
-                          ),
-                        ),
-                        SizedBox(
-                          height: 20.0,
-                        ),
-                        RaisedButton(
-                          onPressed: () {},
-                          child: Container(
-                              height: 50.0,
-                              width: MediaQuery.of(context).size.width * 0.8,
-                              child: Center(
-                                  child: Text(
-                                'INGRESAR',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontFamily: 'quicksand',
-                                    fontWeight: FontWeight.bold),
-                              ))),
-                          shape: StadiumBorder(),
-                          color: Color(0xff00B0FF),
-                        ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        Text(
-                          'Copyright @2020 App para Karen Todos los derechos reservados.',
-                          style: TextStyle(
-                              fontFamily: 'quicksand', fontSize: 12.0),
-                          textAlign: TextAlign.center,
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              )),
-        ],
-      ),
+        )
+      ],
     );
   }
 

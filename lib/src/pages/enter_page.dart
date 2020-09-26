28dbe0de3a5f450d7f87b6f3373958a6ff8bed07
Kevyn Melo <troy.esname@gmail.com>
2020-09-25 19:00:24 -0500

    second commit

--- lib/src/pages/enter_page.dart
+++ lib/src/pages/enter_page.dart
@@ -11,27 +11,42 @@ class _EnterPageState extends State<EnterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          backgroundLogin(),
-          Align(
-              alignment: Alignment.bottomCenter,
-              child: ContainerUpRounded(
-                ht: 0.65,
-                raLeft: 50.0,
-                raRight: 50.0,
-              )),
-          Align(
-              alignment: Alignment.topCenter,
-              child: SvgPicture.asset('assets/images/student.svg')),
-          Align(
-              alignment: Alignment.bottomCenter,
-              child: ContainerUpRounded(
-                ht: 0.57,
-                raLeft: 35.0,
-                raRight: 35.0,
-              )),
-        ],
+      body: SafeArea(
+        child: Stack(
+          children: [
+            backgroundLogin(),
+            Container(
+              height: 200.0,
+              width: double.infinity,
+              child: SvgPicture.asset('assets/images/boys.svg'),
+            ),
+            Align(
+                alignment: Alignment.bottomCenter,
+                child: Stack(
+                  children: [
+                    ContainerUpRounded(
+                      ht: 0.65,
+                      raLeft: 50.0,
+                      raRight: 50.0,
+                    ),
+                    Text(
+                      'CREA TU USUARIO',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          fontFamily: 'quicksand',
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                )),
+            Align(
+                alignment: Alignment.bottomCenter,
+                child: ContainerUpRounded(
+                  ht: 0.57,
+                  raLeft: 35.0,
+                  raRight: 35.0,
+                )),
+          ],
+        ),
       ),
     );
   }
@@ -44,7 +59,7 @@ class _EnterPageState extends State<EnterPage> {
           gradient: LinearGradient(
               begin: Alignment.topRight,
               end: Alignment.bottomLeft,
-              colors: [Color(0xff03A641), Color(0xff0AB14A)]),
+              colors: [Color(0xff1BB273), Color(0xff169D65)]),
         ));
   }
 }
